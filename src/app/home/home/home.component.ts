5e88af7b14fd6fa48ba59edc9ade053ab7cd7019
SztukowskiAdam <adam-sztukowski@wp.pl>
2019-09-15 05:49:22 +0200

    updated component

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,5 +1,7 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {BarecodeScannerLivestreamComponent} from 'ngx-barcode-scanner';
+import {Router} from '@angular/router';
+import {HOME_ROUTING} from '../home.routing';
 
 @Component({
   selector: 'app-home',
@@ -11,17 +13,17 @@ export class HomeComponent implements AfterViewInit {
   @ViewChild(BarecodeScannerLivestreamComponent, {static: false}) barecodeScanner: BarecodeScannerLivestreamComponent;
 
   private barcodeValue: any;
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngAfterViewInit() {
-    this.barecodeScanner.start().then(response => {
-      console.log(response);
-    });
+    this.barecodeScanner.retart();
+    this.barecodeScanner.start();
   }
 
   onValueChanges(result) {
     this.barcodeValue = result.codeResult.code;
     console.log(this.barcodeValue);
+    this.router.navigateByUrl(HOME_ROUTING.PRODUCTS + '/' + this.barcodeValue);
   }
 
 }
