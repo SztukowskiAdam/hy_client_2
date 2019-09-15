67d4bd909d98963bec3b066d2ed5cb8117115004
lukaszNawrocki <rozgrzef92@gmail.com>
2019-09-15 05:49:03 +0200

    Fix of export navbar component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,15 +3,20 @@ import {HomeRoutingModule} from './home-routing.module';
 import {HomeComponent} from './home/home.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {TranslateModule} from '@ngx-translate/core';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   imports: [
     HomeRoutingModule,
-    TranslateModule
+    TranslateModule,
+    CommonModule,
   ],
   declarations: [
     HomeComponent,
     NavbarComponent,
   ],
+  exports: [
+    NavbarComponent,
+  ]
 })
 export class HomeModule {}
